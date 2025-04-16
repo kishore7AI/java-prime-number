5056fef0c93ee43c63e92b78acdcc830e86b8b61
Kishore.M <kishoresabari007@gmail.com>
2025-04-16 22:18:02 +0530

    demo.java

---
+++ demo.java
@@ -0,0 +1,10 @@
+public class demo {
+    public static void main(String[] args) {
+        int num = 33;
+        if (num % 2 == 0 && num % 3 == 0) {
+            System.out.println("prime");
+        } else {
+            System.out.println("not prime");
+        }
+    }
+}
